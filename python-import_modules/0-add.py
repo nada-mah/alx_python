ef778f63d0c0af99fa773c260aac1e62149fb409
nada-mah <cutenada.lara4@gmail.com>
2023-12-04 18:54:34 +0200

    modified:   python-import_modules/0-add.py

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,8 @@
-def main():    
+def main():
+    a=1
+    b=2    
     add = __import__('add_0')
-    print("1 + 2 = {}".format(add.add(1,2)))
+    print("1 + 2 = {}".format(add.add(a,b)))
 
 
 if __name__ == '__main__':
