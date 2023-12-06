7aa2a64e726803a9aab758675cd5821fa03d98da
nada-mah <cutenada.lara4@gmail.com>
2023-12-06 20:00:05 +0200

    modified: 1-print_matrix_integer.py

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,7 +1,7 @@
 def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
-            print("{:2d}".format(matrix[i][j]),end='')
+            print("{:d} ".format(matrix[i][j]),end='')
         print()
     
     
