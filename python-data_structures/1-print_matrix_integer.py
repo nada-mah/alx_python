0d90561dc5b8b52ddd0799a3d1b59c853c16223a
nada-mah <cutenada.lara4@gmail.com>
2023-12-06 20:55:39 +0200

    modified:1-print_matrix_integer.py

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,8 +1,9 @@
 def print_matrix_integer(matrix=[[]]):
+    if not any(matrix):
+        print()
+        return
     for i in range(len(matrix)):
         for j in range(len(matrix[i])-1):
             print("{:d}".format(matrix[i][j]),end=' ')
         print("{:d}".format(matrix[i][-1]))
-    
-
-    return None
+        
