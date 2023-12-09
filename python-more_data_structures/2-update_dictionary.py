cfd8c009ddc0537e9c3f820ccbd25c33cc6e3e86
nada-mah <cutenada.lara4@gmail.com>
2023-12-09 13:34:38 +0200

    simple comment for another way

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -1,8 +1,8 @@
 def update_dictionary(a_dictionary, key, value):
     for k, v in a_dictionary.items():
-        # a_dictionary[k] = value
+        a_dictionary.setdefault(k,value)
         if k == key:
            a_dictionary[k] = value
     a_dictionary[key] = value
-    
+    # a_dictionary.setdefault(key,value)
     return a_dictionary
