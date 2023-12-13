63b93c61e32bebf55fce791ba787d6987d709d58
nada-mah <cutenada.lara4@gmail.com>
2023-12-13 19:17:30 +0200

    modified:   python-classes/3-square.py

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -2,12 +2,10 @@
 class Square:
     """Represents a Square with Size 
     size should be an integer and greater than zero"""
-    __size = None
-    
     def __init__(self,size=0):
         if not isinstance(size,int):
             raise TypeError('size must be an integer')
-        elif size <= 0:
+        elif size < 0:
             raise ValueError('size must be >= 0')
         else:
             self.__size = size
@@ -21,7 +19,7 @@ class Square:
     def size(self, value):
         if not isinstance(value,int):
             raise TypeError('size must be an integer')
-        elif value <= 0:
+        elif value < 0:
             raise ValueError('size must be >= 0')
         else:
             self.__size = value
