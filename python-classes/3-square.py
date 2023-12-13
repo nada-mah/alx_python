397895db919b130ace95425629736376b2ce9fa4
nada-mah <cutenada.lara4@gmail.com>
2023-12-13 19:10:02 +0200

    new file:   python-classes/3-square.py

---
+++ python-classes/3-square.py
@@ -0,0 +1,32 @@
+"""This module houses a class that represents a square """
+class Square:
+    """Represents a Square with Size 
+    size should be an integer and greater than zero"""
+    __size = None
+    
+    def __init__(self,size=0):
+        if not isinstance(size,int):
+            raise TypeError('size must be an integer')
+        elif size <= 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
+            
+    @property
+    def size(self):
+        '''public method: Returns the size of the square'''
+        return self.__size
+    
+    @size.setter
+    def size(self, value):
+        if not isinstance(value,int):
+            raise TypeError('size must be an integer')
+        elif value <= 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = value
+
+    def area(self):
+        """Public instance method: Returns the area of the square"""
+        return (self.__size**2)
+        
