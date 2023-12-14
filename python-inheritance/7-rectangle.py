9147f586dd214995a2ac4711c3a1bc34424cae7c
nada-mah <cutenada.lara4@gmail.com>
2023-12-14 21:06:36 +0200

    new file:   python-inheritance/7-rectangle.py

---
+++ python-inheritance/7-rectangle.py
@@ -0,0 +1,16 @@
+BaseGeometry = __import__('5-base_geometry').BaseGeometry
+
+class Rectangle(BaseGeometry):
+    '''rectangle object class inherits from BaseGeometry'''
+    def __init__(self, width, height):
+        self.integer_validator('width', width)
+        self.__width = width
+        self.integer_validator('height', height)
+        self.__height = height
+    
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+    
+    def area(self):
+        ''' return area of rectangle (width*height)'''
+        return self.__width * self.__height
