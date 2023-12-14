aece07567c3c6cc9ee01005fbee14014d466358c
nada-mah <cutenada.lara4@gmail.com>
2023-12-14 20:44:06 +0200

    mod 5-base_geometry.py, add 6-rectangle.py

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -6,7 +6,7 @@ class BaseGeometry:
         raise Exception('area() is not implemented')
     def integer_validator(self, name, value):
         '''Public instance method: that validates value:'''
-        if not isinstance(value,int):
+        if not (type(value) == int):
             raise TypeError('{} must be an integer'.format(name))
         elif value <= 0:
             raise ValueError('{} must be greater than 0'.format(name))
