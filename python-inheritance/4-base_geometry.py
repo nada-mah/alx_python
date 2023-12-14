e304b4d65a83145d199ce9e800415a190eb86de6
nada-mah <cutenada.lara4@gmail.com>
2023-12-14 15:08:44 +0200

    modified:   python-inheritance/4-base_geometry.py

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -1,5 +1,6 @@
 """def BaseGeometry class"""
 class BaseGeometry:
+    '''Base class for Geometry'''
     def area(self):
         """Public instance method: def area(self): that raises an Exception with the message area() is not implemented"""
         raise Exception('area() is not implemented')
