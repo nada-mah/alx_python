e8adc7463c432bc7bad7504ff213173d225e2145
nada-mah <cutenada.lara4@gmail.com>
2023-12-14 20:22:31 +0200

    new file:   python-inheritance/5-base_geometry.py

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,13 @@
+"""def BaseGeometry class"""
+class BaseGeometry:
+    '''Base class for Geometry'''
+    def area(self):
+        """Public instance method: def area(self): that raises an Exception with the message area() is not implemented"""
+        raise Exception('area() is not implemented')
+    def integer_validator(self, name, value):
+        '''Public instance method: that validates value:'''
+        if not isinstance(value,int):
+            raise TypeError('{} must be an integer'.format(name))
+        elif value <= 0:
+            raise ValueError('{} must be greater than 0'.format(name))
+
