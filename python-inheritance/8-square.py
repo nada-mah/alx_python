d149eca636f8e11b10fa37ab8444aa3ec1f291ef
nada-mah <cutenada.lara4@gmail.com>
2023-12-14 21:24:14 +0200

    new file:   python-inheritance/8-square.py

---
+++ python-inheritance/8-square.py
@@ -0,0 +1,14 @@
+"""this module houses the square class that inherts from rectangle class"""
+
+Rectangle = __import__('7-rectangle').Rectangle
+
+class Square(Rectangle):
+    """A square class that inherits from rectangle class"""
+    def __init__(self, size):
+        self.integer_validator('size', size)
+        self.__size = size
+        super().__init__(size, size)
+    def area(self):
+        """Returns the area of the square"""
+        return self.__size**2
+    
