11def20decc37e623dc3eba7d87a1b1e5fd45bd0
nada-mah <cutenada.lara4@gmail.com>
2023-12-17 22:13:49 +0200

    modified:   python-inheritance/5-base_geometry.py

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,6 +1,8 @@
 """def BaseGeometry class"""
 class BaseGeometry:
     '''Base class for Geometry'''
+    def __dir__ (cls):
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
     def area(self):
         """Public instance method: def area(self): that raises an Exception with the message area() is not implemented"""
         raise Exception('area() is not implemented')
