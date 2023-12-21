040b3a72b1f35a61acfacb620116622a837adf72
nada-mah <cutenada.lara4@gmail.com>
2023-12-21 15:34:55 +0200

    fix URL

--- python-network_1/5-json_api.py
+++ python-network_1/5-json_api.py
@@ -5,7 +5,7 @@ if len(sys.argv)> 1:
     q = sys.argv[1]
 else:
     q=""
-URL = 'http://192.168.43.206:5000/search_user'
+URL = 'http://0.0.0.0:5000/search_user'
 
 res = requests.post(URL, data={'q': q })
 try:
