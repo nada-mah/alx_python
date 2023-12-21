37a48a65626327f5b2f7918249423ac9ba08f4ad
nada-mah <cutenada.lara4@gmail.com>
2023-12-21 15:28:28 +0200

    modified:   python-network_1/5-json_api.py

--- python-network_1/5-json_api.py
+++ python-network_1/5-json_api.py
@@ -5,8 +5,15 @@ if len(sys.argv)> 1:
     q = sys.argv[1]
 else:
     q=""
-URL = 'http://0.0.0.0:5000/search_user'
+URL = 'http://192.168.43.206:5000/search_user'
 
 res = requests.post(URL, data={'q': q })
-x=res.json()
-print("[{}] {}".format(x["id"], x["name"]))
+try:
+    x=res.json()
+    if not x :
+        print("No result")
+    else:
+        print("[{}] {}".format(x["id"], x["name"]))
+except Exception as e:
+    print("Not a valid JSON")
+
