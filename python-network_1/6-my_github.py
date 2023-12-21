5cde19c4e4c03ff97ed216dd700388c608406e10
nada-mah <cutenada.lara4@gmail.com>
2023-12-21 16:07:43 +0200

    new file:   python-network_1/6-my_github.py

---
+++ python-network_1/6-my_github.py
@@ -0,0 +1,15 @@
+import requests 
+import sys 
+
+if len(sys.argv)> 1:
+    username = sys.argv[1]
+    password = sys.argv[2]
+
+URL='https://api.github.com/user'
+# res = requests.get(URL, auth=(username,password))
+try:
+    res = requests.get(URL, auth=(username,password))
+    res_json= res.json()
+    print(res_json['id'])
+except Exception as e:
+    print('None')
