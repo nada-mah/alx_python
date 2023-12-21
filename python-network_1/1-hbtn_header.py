43045470b2dc572b184ae2729bf4f2890ad9d208
nada-mah <cutenada.lara4@gmail.com>
2023-12-21 13:50:54 +0200

    task 1

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -3,6 +3,6 @@ from sys import argv
 
 if len(argv)> 1:
     URL = argv[1]
-    res = requests.get(URL)
-
-    print(res.headers['X-Request-Id'])
+    
+res = requests.get(URL)
+print(res.headers['X-Request-Id'])
