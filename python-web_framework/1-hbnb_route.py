a8c2d703a0a06168c9d5b9f5942403e1aecd1797
nada-mah <cutenada.lara4@gmail.com>
2024-01-10 14:45:04 +0200

    modfied doc of task 0 and added task 1

--- python-web_framework/1-hbnb_route.py
+++ python-web_framework/1-hbnb_route.py
@@ -2,12 +2,16 @@
 from flask import Flask
 
 app = Flask(__name__)
+
 """create route at / display --> “Hello HBNB!”"""
 @app.route('/',strict_slashes=False)
 def hello():
-    
     return "Hello HBNB!"
 
+"""create route at /hbnb display --> “HBNB”"""
+@app.route('/hbnb',strict_slashes=False)
+def hbnb():
+    return "HBNB"
 
 if __name__ == '__main__':
     '''run flask on 0.0.0.0, port 5000'''
