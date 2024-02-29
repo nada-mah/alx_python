d7cefa8fb99f4e66c0aded753e8e880bab1dbdce
nada-mah <cutenada.lara4@gmail.com>
2024-02-29 13:24:19 +0200

    Task 2 api

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -2,5 +2,37 @@
 uses the informatiom given to write it to a json file
 """
 
+import json
+import requests 
+from sys import argv
+
+def get_info(id):
+    url1 = f'https://jsonplaceholder.typicode.com/users/{id}/todos'
+    empurl= f'https://jsonplaceholder.typicode.com/users/{id}'
+    # get todo tasks
+    res1 = requests.get(url1)
+    data1 = res1.json()
+    # get employee information
+    res2 = requests.get(empurl)
+    employeedata = res2.json()
+
+    """Requred data"""
+    USER_ID = employeedata['id']
+    USERNAME = employeedata['username']
+    TASK_COMPLETED_STATUS = ''
+    TOTAL_NUMBER_OF_TASKS = len(data1)
+    TASK_TITLE = ''
+    tasks = []
+    for i in range(len(data1)):
+        """loop to add task status and title"""
+        TASK_COMPLETED_STATUS = data1[i]['completed']
+        TASK_TITLE = data1[i]['title']
+        tasks.append({"task": TASK_TITLE, "completed": TASK_COMPLETED_STATUS, "username":USERNAME})
+        
+    dictionary={"USER_ID": tasks}
+    with open(f'{USER_ID}.json', 'w', newline='') as file:
+        # write dictionary to json file
+        json.dump(dictionary , file)
+
 if __name__ == "__main__":
-    (int(sys.argv[1]))
+    get_info(int(argv[1]))
