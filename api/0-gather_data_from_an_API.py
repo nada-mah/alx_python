8fe182dc7110b72401dcc1671898bbafa50d0be2
nada-mah <cutenada.lara4@gmail.com>
2024-02-29 11:56:41 +0200

    Task 0 api

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,26 @@
+from sys import argv
+import requests 
+
+id = argv[1]
+url1 = f'https://jsonplaceholder.typicode.com/users/{id}/todos'
+empurl= f'https://jsonplaceholder.typicode.com/users/{id}'
+
+res1 = requests.get(url1)
+data1 = res1.json()
+
+res2 = requests.get(empurl)
+data2 = res2.json()
+
+EMPLOYEE_NAME = data2['name']
+NUMBER_OF_DONE_TASKS = 0
+TOTAL_NUMBER_OF_TASKS = len(data1)
+TASK_TITLE = []
+
+for i in range(TOTAL_NUMBER_OF_TASKS):
+    if(data1[i]['completed'])==True:
+        NUMBER_OF_DONE_TASKS+=1
+        TASK_TITLE.append(data1[i]['title'])
+
+print(f'Employee {EMPLOYEE_NAME} is done with tasks({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):')
+for task in TASK_TITLE:
+    print(f'\t {task}')
