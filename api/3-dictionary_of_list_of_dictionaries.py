647688e24af83c308be49efd90b0f7a07a79a029
nada-mah <cutenada.lara4@gmail.com>
2024-03-03 15:53:15 +0200

    test

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,37 +1,73 @@
-"""This module makes 2 api requests and 
-uses the informatiom given to write it to a json file
-"""
-
 import json
-import requests 
+import requests
+import sys
 
-def get_info():
-    dictionary={}
-    empurl= f'https://jsonplaceholder.typicode.com/users'
-    
-    # get employee information
-    res2 = requests.get(empurl)
-    employeedata = res2.json()
-    
-    for employee in employeedata:
-        USER_ID = employee['id']
-        USERNAME = employee['username']
-        todo_URL = f'https://jsonplaceholder.typicode.com/users/{USER_ID}/todos'
-        
-        res1 = requests.get(todo_URL)
-        data1 = res1.json()
-        tasks = []
+def get_all_users():
+    """
+    Fetches all users from the API.
 
-        for i in range(len(data1)):
-            """loop to add task status and title"""
-            TASK_COMPLETED_STATUS = data1[i]['completed']
-            TASK_TITLE = data1[i]['title']
-            tasks.append({ "username":USERNAME, "task": TASK_TITLE, "completed": TASK_COMPLETED_STATUS})
-        
-        dictionary[USER_ID] =tasks
-    with open('todo_all_employees.json', 'w') as file:
-        # write dictionary to json file
-        json.dump(dictionary , file,indent=2)
+    Returns:
+    - dict: Dictionary containing user information.
+    """
+    # Replace the URL with the actual API endpoint for users
+    api_url = "https://jsonplaceholder.typicode.com/users"
+    response = requests.get(api_url)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Error: Unable to fetch user information")
+        sys.exit(1)
+
+def get_all_tasks():
+    """
+    Fetches tasks for all users from the API.
+
+    Returns:
+    - dict: Dictionary containing tasks for all users.
+    """
+    # Replace the URL with the actual API endpoint for tasks
+    api_url = "https://jsonplaceholder.typicode.com/todos"
+    response = requests.get(api_url)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Error: Unable to fetch tasks for all users")
+        sys.exit(1)
+
+def export_to_json(users, tasks):
+    """
+    Exports all user tasks to a JSON file.
+
+    Parameters:
+    - users (dict): Dictionary containing user information.
+    - tasks (dict): Dictionary containing tasks for all users.
+    """
+    filename = "todo_all_employees.json"
+    data = {}
+
+    for user in users:
+        user_id = str(user["id"])
+        username = user["username"]
+        user_tasks = []
+
+        for task in tasks:
+            if task["userId"] == user["id"]:
+                user_tasks.append({
+                    "username": username,
+                    "task": task["title"],
+                    "completed": task["completed"]
+                })
+
+        data[user_id] = user_tasks
+
+    with open(filename, "w") as json_file:
+        json.dump(data, json_file, indent=2)
+
+    print(f"Data exported to {filename}")
 
 if __name__ == "__main__":
-    get_info()
+    users = get_all_users()
+    tasks = get_all_tasks()
+    export_to_json(users, tasks)
