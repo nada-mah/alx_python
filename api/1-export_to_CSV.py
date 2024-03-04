f6c972c6d3b8fa2c0489051320b766af77771bd9
nada-mah <cutenada.lara4@gmail.com>
2024-03-04 11:42:41 +0200

    Task 1 api

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -2,6 +2,8 @@ import csv
 import requests 
 from sys import argv
 
+from sqlalchemy import null
+
 def get_task(id):
     url1 = f'https://jsonplaceholder.typicode.com/users/{id}/todos'
     empurl= f'https://jsonplaceholder.typicode.com/users/{id}'
@@ -12,18 +14,18 @@ def get_task(id):
     res2 = requests.get(empurl)
     employeedata = res2.json()
 
-    USER_ID = employeedata['id']
+    USER_ID = str(employeedata['id']) 
     USERNAME = employeedata['username']
     TASK_COMPLETED_STATUS = ''
     TOTAL_NUMBER_OF_TASKS = len(data1)
     TASK_TITLE = ''
 
     with open(f'api/{id}.csv', 'w') as file:
-        writer = csv.writer(file)
+        writer = csv.writer(file,quotechar="'")
         for i in range(len(data1)):
             TASK_COMPLETED_STATUS = data1[i]['completed']
             TASK_TITLE = data1[i]['title']
-            writer.writerow([USER_ID,USERNAME,TASK_COMPLETED_STATUS,TASK_TITLE])
+            writer.writerow([ '"'+USER_ID+'"', '"'+USERNAME+'"', '"'+str(TASK_COMPLETED_STATUS)+'"', '"'+TASK_TITLE+'"' ])
 if __name__ == "__main__":
     for id in range(1,11):
         get_task(str(id))
